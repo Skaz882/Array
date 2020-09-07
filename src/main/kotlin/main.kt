36fa3b06feac9d83ecf6be242cb7dbb8f88c4d90
Sarah Kazmierczak <skaz88@gmail.com>
2020-09-06 19:04:13 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+fun main (){
+    val arrayOf = arrayOf("ice cream", "chocolate sauce", "cherries", "whipped cream", "spoons", "bowls")
+
+    for (element in arrayOf){
+        println(element)
+    }
+}
